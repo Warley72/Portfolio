6367293b9df36b3f6a3d54424ffb6358cdebffd5
carlos warley <carloswarley0@gmail.com>
2023-12-08 17:15:20 -0300

    tech-commit

--- src/app/page.js
+++ src/app/page.js
@@ -3,11 +3,13 @@ import React from "react";
 import Main from "./components/Main";
 
 import './styles/global.sass'
+import Technologies from "./components/Technologies";
 
 export default function Home() {
   return (
     <div>
       <Main />
+      <Technologies />
     </div>
   );
 }
